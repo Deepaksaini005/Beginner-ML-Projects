a4ba0b4e70457be4938d13a246b18df876632adc
Healthcare User <user@example.com>
2026-08-10 16:19:28 +0530

    pushing the chnages

--- module/add.py
+++ module/add.py
@@ -1,10 +1,11 @@
 
-from ownmodule import add, sub, mul, div
+from ownmodule import add, sub, mul, div  # this  is a relative import and a own module creation 
+
 
 
 print(add(3,4))
 print(sub(3,4))
-print(mul(3,4))
+print(mul(3,4)) # 12
 print(div(3,4))
 
 
